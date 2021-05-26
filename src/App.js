7db30e36071eedb15a61cbb49df8093a74e4f0f6
arvindkumaar <arvind.tomar@gmail.com>
2021-05-26 15:24:04 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 function App() {
   return (
-    <div >
-     Phone Directory
+    <div>
+      {/*<div className= "header">
+      Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+        <span> Name </span> <br/>
+        <span> Phone </span>
+  </div>*/}
+  <label htmlFor = "name">Name</label>
+  <input id = "name" type= "text" placeholder = "Enter Here" defaultValue= "Arvind"/>
     </div>
+    
   );
 }
 
